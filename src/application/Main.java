697f159f95838cb69890aafd43ef6704f22809ba
yago rocha <yagodasilvarocha30@gmail.com>
2025-01-30 14:58:04 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,15 @@
 package application;
 
+import model.DaoFactory;
+import model.SellerDao;
 import model.entities.Department;
 
 public class Main {
     public static void main(String[] args) {
 
         Department obj = new Department(1, "books");
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(obj);
     }
---
+++ src/model/DaoFactory.java
@@ -0,0 +1,14 @@
+package model;
+
+import model.dao.impl.SellerDaoJDBC;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class DaoFactory {
+
+    public static SellerDao createSellerDao() {
+        return new SellerDaoJDBC();
+
+        }
+    }
