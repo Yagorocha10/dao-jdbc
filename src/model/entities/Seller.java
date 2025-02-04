437fd3ec2b642982da1fcb03abfc30cfecfc7c3c
yago rocha <yagodasilvarocha30@gmail.com>
2025-02-04 14:07:45 -0300

    insert implementation

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -11,6 +11,19 @@ public class Seller {
     private Double baseSalary;
     private Department department;
 
+    public Seller() {
+
+    }
+
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
+
     public Integer getId() {
         return id;
     }
